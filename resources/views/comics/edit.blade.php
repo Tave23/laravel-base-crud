16708ea45222beb316f97730c88a957f4264e8fb
Davide Tavella <tavella01@gmail.com>
2022-01-31 17:19:29 +0100

    validazione completa anche in edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -27,7 +27,8 @@
 
          <div class="mb-3">
            <label for="title" class="form-label">Titolo</label>
-           <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+           <input type="text" class="form-control @error('title') is-invalid @enderror"
+            id="title" name="title" value="{{ old('title', $comic->title) }}">
            {{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
             @error('title')
                <p class="errore_nel_form">
@@ -39,7 +40,8 @@
          
          <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
-            <textarea style="height: 200px" class="form-control" id="title" name="description">{{ $comic->description }}</textarea>
+            <textarea style="height: 200px" class="form-control @error('description') is-invalid @enderror"
+            id="title" name="description">{{ old('description', $comic->description) }}</textarea>
             {{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
             @error('description')
                <p class="errore_nel_form">
@@ -51,7 +53,8 @@
 
          <div class="mb-3">
             <label for="thumb" class="form-label">Immagine copertina</label>
-            <input type="text" class="form-control" name="thumb" id="title" value="{{ $comic->thumb }}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror"
+             name="thumb" id="title" value="{{ old('thumb', $comic->thumb) }}">
             {{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
             @error('thumb')
                <p class="errore_nel_form">
@@ -63,7 +66,8 @@
 
          <div class="mb-3">
             <label for="price" class="form-label">Prezzo</label>
-            <input type="number" class="form-control" name="price" id="title" value="{{ $comic->price }}">
+            <input type="number" class="form-control @error('price') is-invalid @enderror"
+             name="price" id="title" value="{{ old('price', $comic->price) }}">
             {{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
             @error('price')
                <p class="errore_nel_form">
@@ -75,7 +79,8 @@
 
          <div class="mb-3">
             <label for="series" class="form-label">Serie</label>
-            <input type="text" class="form-control" name="series" id="title" value="{{ $comic->series }}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" 
+            name="series" id="title" value="{{ old('series', $comic->series) }}">
             {{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
             @error('series')
                <p class="errore_nel_form">
@@ -87,7 +92,8 @@
 
          <div class="mb-3">
             <label for="sale_date" class="form-label">Data di uscita</label>
-            <input type="text" class="form-control" name="sale_date" id="title" value="{{ $comic->sale_date }}">
+            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" 
+            name="sale_date" id="title" value="{{ old('sale_date', $comic->sale_date) }}">
             {{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
             @error('sale_date')
                <p class="errore_nel_form">
@@ -99,7 +105,9 @@
 
          <div class="mb-3">
             <label for="type" class="form-label">Tipo</label>
-            <input type="text" class="form-control" name="type" id="title" value="{{ $comic->type }}">{{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
+            <input type="text" class="form-control @error('type') is-invalid @enderror" 
+            name="type" id="title" value="{{ old('type', $comic->type) }}">
+            {{-- se esiste l'errore del 'titolo' allora stampa ciò che c'è dentro --}}
             @error('type')
                <p class="errore_nel_form">
                   {{-- parametro automatico con cui viene passato il messaggio d'errore --}}
